2667dfa368bbd9d4ae8d8fc43732197b5bcd7627
Sangharshmuneshwar <93377050+Sangharshmuneshwar@users.noreply.github.com>
2022-03-02 13:58:48 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import { Header } from "./Component/Header";
+import From from "./Component/From";
+
+function App() {
+  return (
+    <section>
+      <div className="contaner">
+        <Header />
+        <From />
+      </div>
+    </section>
+  );
+}
+
+export default App;
